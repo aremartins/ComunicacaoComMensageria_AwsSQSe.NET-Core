8ef563aa6603ca9c9501d382eb808c7791692de0
ArethaMartins <aretha.martins@gestaosat.com>
2021-10-13 09:42:33 -0300

    first commit

---
+++ SQS.Consumidor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Consumidor
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+            var request = new ReceiveMessageRequest
+            {
+                QueueUrl = "https://sqs.sa-east-1.amazonaws.com/796779732004/teste2"
+            };
+
+
+            while (true)
+            {
+                var response = await client.ReceiveMessageAsync(request);
+
+                foreach (var mensagem in response.Messages)
+                {
+                    Console.WriteLine(mensagem.Body);
+                    await client.DeleteMessageAsync("https://sqs.sa-east-1.amazonaws.com/796779732004/teste2", mensagem.ReceiptHandle);
+                }
+            }
+        }
+    }
+}
---
+++ SQS.Fornecedor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Fornecedor
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+            var request = new SendMessageRequest
+            {
+                QueueUrl = "https://sqs.sa-east-1.amazonaws.com/796779732004/teste2",
+                MessageBody = "Mensagem recebida com sucesso!"
+            };
+            
+            await client.SendMessageAsync(request);
+        }
+    }
+}
